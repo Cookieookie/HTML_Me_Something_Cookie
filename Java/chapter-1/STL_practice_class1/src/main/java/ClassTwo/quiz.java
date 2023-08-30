dbdf5054088fcf95512256102357cf2fb5f37eaa
Cookie Van <cookievan03@gmail.com>
2023-08-29 21:44:38 -0500

    adding to satisfy the assignment requirement

---
+++ Java/chapter-1/STL_practice_class1/src/main/java/ClassTwo/quiz.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+// From class 2 - 8/18/2023
+public class quiz {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("This Summer cohort likes summer. (T/F)? ");
+        String answer = input.nextLine();
+
+        if(answer.equals("F")) { // (if answer == "F") will not work here
+            System.out.println("correct");
+        } else {
+            System.out.println("incorrect");
+        }
+
+        System.out.println("You said: " + answer);
+    }
+}
