a46a7a495781469f0972af2d4d360dd828c00714
Cookie Van <cookievan03@gmail.com>
2023-10-19 13:26:42 -0500

    completed bootstrap part of chapter 13

---
+++ Java/chapter-1/codingevents/codingevents/src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,37 @@
+package org.launchcode.codingevents.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    //this static list belongs to the entire class rather than instances of the class. This allows for it to be shared by every instance of our event controller object.
+    private static List<String> events = new ArrayList<>();
+    @GetMapping
+    public String displayAllEvents(Model model) {
+        model.addAttribute("events", events);
+        return "events/index";
+    }
+
+    //lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
+
+    //lives at /events/create too - ok to put at the same place as the method above bc they handle different requests.
+    @PostMapping("create")
+    public String createEvent(@RequestParam String eventName) {
+        events.add(eventName);
+        return "redirect:/events";
+    }
+}
